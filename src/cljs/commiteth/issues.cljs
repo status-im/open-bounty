161b5b805233f0f6a4f1b3515188940ef3e65f57
kagel <dimax4@gmail.com>
2016-09-26 01:56:00 +0300

    Remove assignee and comments count

--- src/cljs/commiteth/issues.cljs
+++ src/cljs/commiteth/issues.cljs
@@ -54,37 +54,12 @@
         {:d
          "M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"}]]]]
     [:div.float-left.col-9.p-3.lh-condensed
-     [:span.mr-2.float-right.text-gray.text-small
-      (str " #" issue-number)]
      [:a.box-row-link.h4
       {:href (issue-url owner repo issue-number)} title]
+     [:span.gh-header-number
+      (str " #" issue-number)]
      [:div.mt-1.text-small.text-gray
-      (repo-link owner repo)]]
-    [:div.float-right.col-2
-     [:div.float-left.col-7.pt-3.pr-3.text-right
-      [:div.avatar-stack.tooltipped.tooltipped-multiline.tooltipped-s
-       {:aria-label "Assigned to obscuren"}
-       [:a.avatar
-        {:href       "#",
-         :aria-label "View everything assigned to obscuren"}
-        [:img.from-avatar
-         {:alt    "obscuren",
-          :src    "https://avatars3.githubusercontent.com/u/6264126",
-          :width  "20",
-          :height "20"}]]]]
-     [:div.float-right.col-5.no-wrap.pt-3.pr-3.text-right
-      [:a.muted-link
-       {:href "#", :aria-label "1 comment"}
-       [:svg.octicon.octicon-comment.v-align-middle
-        {:aria-hidden "true",
-         :height      "16",
-         :version     "1.1",
-         :viewBox     "0 0 16 16",
-         :width       "16"}
-        [:path
-         {:d
-          "M14 1H2c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1h2v3.5L7.5 11H14c.55 0 1-.45 1-1V2c0-.55-.45-1-1-1zm0 9H7l-2 2v-2H2V2h12v8z"}]]
-       [:span.text-small "1"]]]]]])
+      (repo-link owner repo)]]]])
 
 (defn issues-list-table
   [issues-path issue-row-fn]
