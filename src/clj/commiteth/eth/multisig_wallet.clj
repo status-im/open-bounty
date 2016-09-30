369d45ef516492c44f94b474f88ce22afda40b34
kagel <dimax4@gmail.com>
2016-09-30 20:02:28 +0300

    Fix multisig wallet `execute` method

--- src/clj/commiteth/eth/multisig_wallet.clj
+++ src/clj/commiteth/eth/multisig_wallet.clj
@@ -9,4 +9,4 @@
 (defn execute
   [contract to value]
   (log/debug "multisig.execute(contract, to, value)" contract to value)
-  (eth/execute (eth/eth-account) contract "0xb61d27f6" to value))
+  (eth/execute (eth/eth-account) contract "0xb61d27f6" to value "0x60" "0x0" "0x0"))
