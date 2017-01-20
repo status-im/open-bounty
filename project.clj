0be96b26cdef7f7fdc89969629a05f9b05fc0faa
Teemu Patja <tp@iki.fi>
2017-01-21 00:11:17 +0200

    Update dependencies and clean up project file

--- project.clj
+++ project.clj
@@ -1,68 +1,66 @@
 (defproject commiteth "0.1.0-SNAPSHOT"
 
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Ethereum bounty bot for Github"
+  :url "https://commiteth.com"
 
-  :dependencies [[metosin/compojure-api "1.1.6"]
-                 [re-frame "0.8.0"]
+  :dependencies [[metosin/compojure-api "1.1.10"]
+                 [re-frame "0.9.1"]
                  [cljs-ajax "0.5.8"]
                  [secretary "1.2.3"]
                  [reagent-utils "0.2.0"]
                  [reagent "0.6.0"]
-                 [org.clojure/clojurescript "1.9.229" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.293" :scope "provided"]
                  [org.clojure/clojure "1.8.0"]
-                 [selmer "1.0.7"]
-                 [markdown-clj "0.9.89"]
+                 [selmer "1.10.5"]
+                 [markdown-clj "0.9.91"]
                  [ring-middleware-format "0.7.0"]
-                 [metosin/ring-http-response "0.8.0"]
+                 [metosin/ring-http-response "0.8.1"]
                  [bouncer "1.0.0"]
-                 [org.webjars/bootstrap "4.0.0-alpha.3"]
-                 [org.webjars/font-awesome "4.6.3"]
+                 [org.webjars/bootstrap "4.0.0-alpha.6"]
+                 [org.webjars/font-awesome "4.7.0"]
                  [org.webjars/bootstrap-social "5.0.0"]
-                 [org.webjars.bower/tether "1.3.3"]
+                 [org.webjars.bower/tether "1.4.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [compojure "1.5.1"]
-                 [http-kit "2.1.18"]
+                 [compojure "1.5.2"]
+                 [http-kit "2.2.0"]
                  [ring/ring-json "0.4.0"]
                  [ring-webjars "0.1.1"]
-                 [ring/ring-defaults "0.2.1"]
-                 [mount "0.1.10"]
-                 [cprop "0.1.9"]
+                 [ring/ring-defaults "0.2.2"]
+                 [ring/ring-codec "1.0.1"]
+                 [mount "0.1.11"]
+                 [cprop "0.1.10"]
                  [org.clojure/tools.cli "0.3.5"]
                  [luminus-nrepl "0.1.4"]
-                 [buddy "1.0.0"]
-                 [buddy/buddy-auth "1.1.0"]
-                 [luminus-migrations "0.2.6"]
-                 [conman "0.6.0"]
-                 [org.postgresql/postgresql "9.4.1209"]
+                 [buddy "1.2.0"]
+                 [buddy/buddy-auth "1.3.0"]
+                 [luminus-migrations "0.2.9"]
+                 [conman "0.6.3"]
+                 [org.postgresql/postgresql "9.4.1212"]
                  [org.webjars/webjars-locator-jboss-vfs "0.1.0"]
-                 [luminus-immutant "0.2.2"]
+                 [luminus-immutant "0.2.3"]
                  [clj.qrgen "0.4.0"]
-                 [digest "1.4.4"]
+                 [digest "1.4.5"]
                  [tentacles "0.5.1"]]
 
   :min-lein-version "2.0.0"
-
-  :jvm-opts ["-server" "-Dconf=.lein-env"]
   :source-paths ["src/clj" "src/cljc"]
   :resource-paths ["resources" "target/cljsbuild"]
   :target-path "target/%s/"
   :main commiteth.core
-  :migratus {:store :database :db ~(get (System/getenv) "DATABASE_URL")}
+  :migratus {:store :database
+             :migration-dir "resources/migrations"
+             :db ~(get (System/getenv) "DATABASE_URL")}
 
   :plugins [[lein-cprop "1.0.1"]
             [migratus-lein "0.4.1"]
             [lein-cljsbuild "1.1.3"]
-            [lein-immutant "2.1.0"]
             [lein-sassc "0.10.4"]
             [lein-npm "0.6.2"]
             [lein-auto "0.1.2"]]
-  :immutant {:war {:name         "ROOT"
-                   :destination  "/opt/wildfly/standalone/deployments"
-                   :context-path "/"}}
-  :ring {:destroy commiteth.scheduler/stop-scheduler}
 
-  ;:npm {:dependencies ["primer-css" "latest"]}
+  :user {:plugins  [[cider/cider-nrepl "0.15.0-SNAPSHOT"]]}
+
+  :ring {:destroy commiteth.scheduler/stop-scheduler}
 
   :sassc
   [{:src         "resources/scss/main.scss"
@@ -74,7 +72,7 @@
   {"sassc" {:file-pattern #"\.(scss|sass)$" :paths ["resources/scss"]}}
 
   :clean-targets ^{:protect false}
-[:target-path [:cljsbuild :builds :app :compiler :output-dir] [:cljsbuild :builds :app :compiler :output-to]]
+  [:target-path [:cljsbuild :builds :app :compiler :output-dir] [:cljsbuild :builds :app :compiler :output-to]]
   :figwheel
   {:http-server-root "public"
    :nrepl-port       7002
@@ -86,16 +84,16 @@
   {:uberjar       {:omit-source    true
                    :prep-tasks     ["compile" ["cljsbuild" "once" "min"]]
                    :cljsbuild
-                                   {:builds
-                                    {:min
-                                     {:source-paths ["src/cljc" "src/cljs" "env/prod/cljs"]
-                                      :compiler
-                                                    {:output-to     "target/cljsbuild/public/js/app.js"
-                                                     :externs       ["react/externs/react.js"]
-                                                     :optimizations :advanced
-                                                     :pretty-print  false
-                                                     :closure-warnings
-                                                                    {:externs-validation :off :non-standard-jsdoc :off}}}}}
+                   {:builds
+                    {:min
+                     {:source-paths ["src/cljc" "src/cljs" "env/prod/cljs"]
+                      :compiler
+                      {:output-to     "target/cljsbuild/public/js/app.js"
+                       :externs       ["react/externs/react.js"]
+                       :optimizations :advanced
+                       :pretty-print  false
+                       :closure-warnings
+                       {:externs-validation :off :non-standard-jsdoc :off}}}}}
 
 
                    :aot            :all
@@ -106,30 +104,30 @@
    :dev           [:project/dev :profiles/dev]
    :test          [:project/test :profiles/test]
 
-   :project/dev   {:dependencies   [[prone "1.1.1"]
+   :project/dev   {:dependencies   [[prone "1.1.4"]
                                     [ring/ring-mock "0.3.0"]
-                                    [ring/ring-devel "1.5.0"]
+                                    [ring/ring-devel "1.5.1"]
                                     [pjstadig/humane-test-output "0.8.1"]
                                     [doo "0.1.7"]
-                                    [binaryage/devtools "0.8.1"]
-                                    [figwheel-sidecar "0.5.7"]
+                                    [binaryage/devtools "0.8.3"]
+                                    [figwheel-sidecar "0.5.8"]
+                                    [org.clojure/tools.nrepl "0.2.12"]
                                     [com.cemerick/piggieback "0.2.2-SNAPSHOT"]]
                    :plugins        [[com.jakemccrary/lein-test-refresh "0.14.0"]
                                     [lein-doo "0.1.7"]
-                                    [lein-figwheel "0.5.7"]
-                                    [org.clojure/clojurescript "1.9.225"]]
+                                    [lein-figwheel "0.5.7"]]
                    :cljsbuild
-                                   {:builds
-                                    {:app
-                                     {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
-                                      :compiler
-                                                    {:main          "commiteth.app"
-                                                     :asset-path    "/js/out"
-                                                     :output-to     "target/cljsbuild/public/js/app.js"
-                                                     :output-dir    "target/cljsbuild/public/js/out"
-                                                     :source-map    true
-                                                     :optimizations :none
-                                                     :pretty-print  true}}}}
+                   {:builds
+                    {:app
+                     {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
+                      :compiler
+                      {:main          "commiteth.app"
+                       :asset-path    "/js/out"
+                       :output-to     "target/cljsbuild/public/js/app.js"
+                       :output-dir    "target/cljsbuild/public/js/out"
+                       :source-map    true
+                       :optimizations :none
+                       :pretty-print  true}}}}
 
 
 
@@ -141,14 +139,14 @@
                                     (pjstadig.humane-test-output/activate!)]}
    :project/test  {:resource-paths ["env/dev/resources" "env/test/resources"]
                    :cljsbuild
-                                   {:builds
-                                    {:test
-                                     {:source-paths ["src/cljc" "src/cljs" "test/cljs"]
-                                      :compiler
-                                                    {:output-to     "target/test.js"
-                                                     :main          "commiteth.doo-runner"
-                                                     :optimizations :whitespace
-                                                     :pretty-print  true}}}}
+                   {:builds
+                    {:test
+                     {:source-paths ["src/cljc" "src/cljs" "test/cljs"]
+                      :compiler
+                      {:output-to     "target/test.js"
+                       :main          "commiteth.doo-runner"
+                       :optimizations :whitespace
+                       :pretty-print  true}}}}
 
                    }
    :profiles/dev  {}
