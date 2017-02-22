66a65277aeb4dcdeeb8d75cb837323272715b663
Teemu Patja <tp@iki.fi>
2017-02-22 22:54:48 +0200

    Remove broken + unneeded /docs route

--- src/clj/commiteth/routes/home.clj
+++ src/clj/commiteth/routes/home.clj
@@ -2,8 +2,7 @@
   (:require [commiteth.layout :as layout]
             [compojure.core :refer [defroutes GET]]
             [ring.util.response :refer [redirect]]
-            [ring.util.http-response :refer [ok header]]
-            [clojure.java.io :as io]))
+            [ring.util.http-response :refer [ok header]]))
 
 (defn home-page [{user-id :id login :login token :token}]
   (layout/render "home.html" {:userId user-id :login login :token token}))
@@ -13,6 +12,4 @@
        (home-page identity))
   (GET "/logout" {session :session}
        (-> (redirect "/")
-           (assoc :session (dissoc session :identity))))
-  (GET "/docs" [] (-> (ok (-> "docs/docs.md" io/resource slurp))
-                      (header "Content-Type" "text/plain; charset=utf-8"))))
+           (assoc :session (dissoc session :identity)))))
