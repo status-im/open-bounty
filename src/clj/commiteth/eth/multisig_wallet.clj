074f510aa5c86c8cd83db8e0ab7c9543180c6554
Teemu Patja <tp@iki.fi>
2017-08-17 13:32:59 +0300

    Un-hardcode another hex method ID

--- src/clj/commiteth/eth/multisig_wallet.clj
+++ src/clj/commiteth/eth/multisig_wallet.clj
@@ -5,7 +5,8 @@
 (defonce confirmation-topic "0xc0ba8fe4b176c1714197d43b9cc6bcf797a4a7461c5fe8d0ef6e184ae7601e51")
 
 (defonce method-ids
-  {:submit-transaction (eth/sig->method-id "submitTransaction(address,uint256,bytes)")})
+  {:submit-transaction (eth/sig->method-id "submitTransaction(address,uint256,bytes)")
+   :withdraw-everything (eth/sig->method-id "withdrawEverything(address)")})
 
 
 (defn execute
@@ -34,11 +35,14 @@
 (defn send-all
   [contract to]
   (log/debug "multisig.send-all(contract, to)" contract to)
-  (eth/execute (eth/eth-account)
-               contract
-               (:submit-transaction method-ids)
-               contract
-               0
-               "0x60"
-               "0x24"
-               (eth/format-call-params "0xf750aaa6" to)))
+  (let [params (eth/format-call-params
+                (:withdraw-everything method-ids)
+                to)]
+    (eth/execute (eth/eth-account)
+                 contract
+                 (:submit-transaction method-ids)
+                 contract
+                 0
+                 "0x60"
+                 "0x24"
+                 params)))
