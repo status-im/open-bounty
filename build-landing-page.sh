a3d07d95b9868eb3a6a8e86343dd2c008f945887
Goran Jovic <goranjovic@gmail.com>
2017-10-31 12:41:51 +0100

    fixed build-build-landing-page.sh and re-run it with github logo

--- build-landing-page.sh
+++ build-landing-page.sh
@@ -4,8 +4,8 @@
 
 echo "Copying index.html, dest, and assets to resources/{templates,public}."
 cp static_langing_page/index.html resources/templates/index.html
-cp -r static_langing_page/dest/ resources/public/dest/
-cp -r static_langing_page/assets/ resources/public/assets/
+cp -r static_langing_page/dest resources/public/
+cp -r static_langing_page/assets resources/public/
 
 
 
