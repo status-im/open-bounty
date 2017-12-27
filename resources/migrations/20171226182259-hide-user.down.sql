bea4321ca4eea71de8483a4fb30f0531051ddd4a
Ivan Grishaev <ivan@grishaev.me>
2017-12-27 18:47:37 +0300

    193 migrations updated

--- resources/migrations/20171226182259-hide-user.down.sql
+++ resources/migrations/20171226182259-hide-user.down.sql
@@ -1,6 +1,35 @@
-begin;
+BEGIN;
 
-alter table users
-    drop column is_hidden;
+ALTER TABLE users
+    DROP COLUMN is_hidden;
 
-commit;
+-- restore the previous version of the view
+CREATE OR REPLACE VIEW "public"."claims_view" AS
+SELECT
+    i.title AS issue_title,
+    i.issue_number,
+    r.repo AS repo_name,
+    r.owner AS repo_owner,
+    COALESCE(u.name, u.login) AS user_name,
+    u.avatar_url AS user_avatar_url,
+    i.payout_receipt,
+    p.updated,
+    i.updated AS issue_updated,
+    i.balance_eth,
+    i.tokens,
+    i.value_usd,
+    p.state AS pr_state,
+    i.is_open AS issue_open,
+    (case when u.address IS NULL THEN false ELSE true END) AS user_has_address
+   FROM issues i,
+    users u,
+    repositories r,
+    pull_requests p
+  WHERE r.repo_id = i.repo_id
+  AND p.issue_id = i.issue_id
+  AND p.user_id = u.id
+  AND i.contract_address IS NOT NULL
+  AND i.comment_id IS NOT NULL
+  ORDER BY p.updated;
+
+COMMIT;
