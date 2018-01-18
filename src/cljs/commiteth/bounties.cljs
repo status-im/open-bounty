11e4a00ac6c9ed9d7fe66e2f9cf06f0c55a7fb63
pablodip <pablodip@gmail.com>
2018-01-18 09:08:37 +0100

    some filter markup and behaviour

--- src/cljs/commiteth/bounties.cljs
+++ src/cljs/commiteth/bounties.cljs
@@ -45,48 +45,90 @@
       [:div.ui.tiny.circular.image
        [:img {:src avatar-url}]]]]))
 
-(defn bounties-filter [name]
+(defn bounties-filter-tooltip [& content]
+  [:div.open-bounties-filter-element-tooltip
+   content])
+
+(defn bounties-filter-tooltip-value-input [label tooltip-open?]
+  [:div.open-bounties-filter-element-tooltip-value-input-container
+   [:div.:input.open-bounties-filter-element-tooltip-value-input-label
+    label]
+   [:input.open-bounties-filter-element-tooltip-value-input
+    {:type     "range"
+     :min      0
+     :max      1000
+     :step     10
+     :on-focus #(reset! tooltip-open? true)}]])
+
+(defn bounties-filter-tooltip-value [tooltip-open?]
+  [bounties-filter-tooltip
+   "$0 - $1000+"
+   [bounties-filter-tooltip-value-input "Min" tooltip-open?]
+   [bounties-filter-tooltip-value-input "Max" tooltip-open?]])
+
+(defn bounties-filter-tooltip-currency []
+  [bounties-filter-tooltip "CURRENCY"])
+
+(defn bounties-filter-tooltip-date []
+  [bounties-filter-tooltip
+   [:div.open-bounties-filter-list
+    (for [t ["Last week" "Last month" "Last 3 months"]]
+      [:div.open-bounties-filter-list-option
+       t])]])
+
+(defn bounties-filter-tooltip-owner [tooltip-open?]
+  [bounties-filter-tooltip
+   [:div.open-bounties-filter-list
+    (for [t ["status-im" "aragon"]]
+      [:div.open-bounties-filter-list-option-checkbox
+       [:label
+        [:input
+         {:type     "checkbox"
+          :on-focus #(reset! tooltip-open? true)}]
+        [:div.text t]]])]])
+
+(defn bounties-filter [name tooltip]
   (let [open? (r/atom false)]
-    (fn [name]
+    (fn [name tooltip]
       [:div.open-bounties-filter-element-container
        {:tab-index 0
+        :on-focus  #(reset! open? true)
         :on-blur   #(reset! open? false)}
        [:div.open-bounties-filter-element
-        {:on-click #(swap! open? not)
-         :class    (when @open? "open-bounties-filter-element-active")}
+        {:on-mouse-down #(swap! open? not)
+         :class         (when @open? "open-bounties-filter-element-active")}
         name]
        (when @open?
-         [:div.open-bounties-filter-element-tooltip
-          "TOOLTIP"])])))
+         [tooltip open?])])))
 
 (defn bounties-filters []
   [:div.open-bounties-filter
-   [bounties-filter "Value"]
-   [bounties-filter "Currency"]
-   [bounties-filter "Date"]
-   [bounties-filter "Owner"]])
+   [bounties-filter "Value" bounties-filter-tooltip-value]
+   [bounties-filter "Currency" bounties-filter-tooltip-currency]
+   [bounties-filter "Date" bounties-filter-tooltip-date]
+   [bounties-filter "Owner" bounties-filter-tooltip-owner]])
 
 (defn bounties-sort []
   (let [open? (r/atom false)]
     (fn []
       (let [current-sorting (rf/subscribe [::db/bounty-sorting-type])]
         [:div.open-bounties-sort
-        {:tab-index 0
-         :on-blur   #(reset! open? false)}
-        [:div.open-bounties-sort-element
-         {:on-click #(swap! open? not)}
-         (db/bounty-sorting-types @current-sorting)
-         [:div.icon-forward-white-box
-          [:img
-           {:src "icon-forward-white.svg"}]]]
-        (when @open?
-          [:div.open-bounties-sort-element-tooltip
-           (for [[sorting-type sorting-name] db/bounty-sorting-types]
-             [:div.open-bounties-sort-type
-              {:on-click #(do
-                            (reset! open? false)
-                            (rf/dispatch [::handlers/set-bounty-sorting-type sorting-type]))}
-              sorting-name])])]))))
+         {:tab-index 0
+          :on-blur   #(reset! open? false)}
+         [:div.open-bounties-sort-element
+          {:on-click #(swap! open? not)}
+          (db/bounty-sorting-types @current-sorting)
+          [:div.icon-forward-white-box
+           [:img
+            {:src "icon-forward-white.svg"}]]]
+         (when @open?
+           [:div.open-bounties-sort-element-tooltip
+            (for [[sorting-type sorting-name] db/bounty-sorting-types]
+              [:div.open-bounties-sort-type
+               {:on-click #(do
+                             (reset! open? false)
+                             (rf/dispatch [::handlers/set-bounty-sorting-type sorting-type]))}
+               sorting-name])])]))))
 
 (defn bounties-list [open-bounties]
   [:div.ui.container.open-bounties-container
