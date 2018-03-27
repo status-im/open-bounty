1d3dd6fbee0ec8bdaece05e9139c2352cfbc9fcb
Vitaliy Vlasov <siphiuel@gmail.com>
2018-03-27 17:28:46 +0300

    Compiled with web3j 3.3.1

--- src/java/commiteth/eth/contracts/Owned.java
+++ src/java/commiteth/eth/contracts/Owned.java
@@ -5,9 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.Future;
 import org.web3j.abi.EventEncoder;
-import org.web3j.abi.EventValues;
 import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.Event;
@@ -16,6 +14,7 @@ import org.web3j.abi.datatypes.Type;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.RemoteCall;
 import org.web3j.protocol.core.methods.request.EthFilter;
 import org.web3j.protocol.core.methods.response.Log;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
@@ -25,20 +24,22 @@ import rx.Observable;
 import rx.functions.Func1;
 
 /**
- * Auto generated code.<br>
- * <strong>Do not modify!</strong><br>
- * Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>, or {@link org.web3j.codegen.SolidityFunctionWrapperGenerator} to update.
+ * <p>Auto generated code.
+ * <p><strong>Do not modify!</strong>
+ * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
+ * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
+ * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
  *
- * <p>Generated with web3j version 2.3.0.
+ * <p>Generated with web3j version 3.3.1.
  */
-public final class Owned extends Contract {
-    private static final String BINARY = "606060405260008054600160a060020a03191633600160a060020a0316179055341561002a57600080fd5b5b6101578061003a6000396000f300606060405263ffffffff7c010000000000000000000000000000000000000000000000000000000060003504166313af403581146100485780638da5cb5b14610069575b600080fd5b341561005357600080fd5b610067600160a060020a0360043516610098565b005b341561007457600080fd5b61007c61011c565b604051600160a060020a03909116815260200160405180910390f35b60005433600160a060020a039081169116146100b357610118565b600054600160a060020a0380831691167f70aea8d848e8a90fb7661b227dc522eb6395c3dac71b63cb59edd5c9899b236460405160405180910390a36000805473ffffffffffffffffffffffffffffffffffffffff1916600160a060020a0383161790555b5b50565b600054600160a060020a0316815600a165627a7a723058206bbc12c5f2dde30ed4acaaf0df4c0bc44e5909ce3b8ab82da7f226137465b3fd0029";
+public class Owned extends Contract {
+    private static final String BINARY = "606060405260008054600160a060020a03191633600160a060020a0316179055341561002a57600080fd5b6101ac806100396000396000f30060606040526004361061004b5763ffffffff7c010000000000000000000000000000000000000000000000000000000060003504166313af403581146100505780638da5cb5b1461007e575b600080fd5b341561005b57600080fd5b61007c73ffffffffffffffffffffffffffffffffffffffff600435166100ba565b005b341561008957600080fd5b610091610164565b60405173ffffffffffffffffffffffffffffffffffffffff909116815260200160405180910390f35b6000543373ffffffffffffffffffffffffffffffffffffffff9081169116146100e257610161565b60005473ffffffffffffffffffffffffffffffffffffffff80831691167f70aea8d848e8a90fb7661b227dc522eb6395c3dac71b63cb59edd5c9899b236460405160405180910390a36000805473ffffffffffffffffffffffffffffffffffffffff191673ffffffffffffffffffffffffffffffffffffffff83161790555b50565b60005473ffffffffffffffffffffffffffffffffffffffff16815600a165627a7a723058206f92b115e641d63f5ea85daf79224b120587e075f9a8eee88ef952eb8c83efc40029";
 
-    private Owned(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+    protected Owned(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
         super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
     }
 
-    private Owned(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+    protected Owned(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
         super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
     }
 
@@ -46,12 +47,13 @@ public final class Owned extends Contract {
         final Event event = new Event("NewOwner", 
                 Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}, new TypeReference<Address>() {}),
                 Arrays.<TypeReference<?>>asList());
-        List<EventValues> valueList = extractEventParameters(event, transactionReceipt);
+        List<Contract.EventValuesWithLog> valueList = extractEventParametersWithLog(event, transactionReceipt);
         ArrayList<NewOwnerEventResponse> responses = new ArrayList<NewOwnerEventResponse>(valueList.size());
-        for (EventValues eventValues : valueList) {
+        for (Contract.EventValuesWithLog eventValues : valueList) {
             NewOwnerEventResponse typedResponse = new NewOwnerEventResponse();
-            typedResponse.old = (Address) eventValues.getIndexedValues().get(0);
-            typedResponse.current = (Address) eventValues.getIndexedValues().get(1);
+            typedResponse.log = eventValues.getLog();
+            typedResponse.old = (String) eventValues.getIndexedValues().get(0).getValue();
+            typedResponse.current = (String) eventValues.getIndexedValues().get(1).getValue();
             responses.add(typedResponse);
         }
         return responses;
@@ -66,33 +68,37 @@ public final class Owned extends Contract {
         return web3j.ethLogObservable(filter).map(new Func1<Log, NewOwnerEventResponse>() {
             @Override
             public NewOwnerEventResponse call(Log log) {
-                EventValues eventValues = extractEventParameters(event, log);
+                Contract.EventValuesWithLog eventValues = extractEventParametersWithLog(event, log);
                 NewOwnerEventResponse typedResponse = new NewOwnerEventResponse();
-                typedResponse.old = (Address) eventValues.getIndexedValues().get(0);
-                typedResponse.current = (Address) eventValues.getIndexedValues().get(1);
+                typedResponse.log = log;
+                typedResponse.old = (String) eventValues.getIndexedValues().get(0).getValue();
+                typedResponse.current = (String) eventValues.getIndexedValues().get(1).getValue();
                 return typedResponse;
             }
         });
     }
 
-    public Future<TransactionReceipt> setOwner(Address _new) {
-        Function function = new Function("setOwner", Arrays.<Type>asList(_new), Collections.<TypeReference<?>>emptyList());
-        return executeTransactionAsync(function);
+    public RemoteCall<TransactionReceipt> setOwner(String _new) {
+        final Function function = new Function(
+                "setOwner", 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(_new)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
     }
 
-    public Future<Address> owner() {
-        Function function = new Function("owner", 
+    public RemoteCall<String> owner() {
+        final Function function = new Function("owner", 
                 Arrays.<Type>asList(), 
                 Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}));
-        return executeCallSingleValueReturnAsync(function);
+        return executeRemoteCallSingleValueReturn(function, String.class);
     }
 
-    public static Future<Owned> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit, BigInteger initialWeiValue) {
-        return deployAsync(Owned.class, web3j, credentials, gasPrice, gasLimit, BINARY, "", initialWeiValue);
+    public static RemoteCall<Owned> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(Owned.class, web3j, credentials, gasPrice, gasLimit, BINARY, "");
     }
 
-    public static Future<Owned> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit, BigInteger initialWeiValue) {
-        return deployAsync(Owned.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "", initialWeiValue);
+    public static RemoteCall<Owned> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(Owned.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "");
     }
 
     public static Owned load(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
@@ -104,8 +110,10 @@ public final class Owned extends Contract {
     }
 
     public static class NewOwnerEventResponse {
-        public Address old;
+        public Log log;
 
-        public Address current;
+        public String old;
+
+        public String current;
     }
 }
