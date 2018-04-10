aaee8a072a9c43c7e001af8188088b2ec80931d8
Vitaliy Vlasov <siphiuel@gmail.com>
2018-04-10 13:59:12 +0300

    Delete rows from pull_requests which map to non-existing issues

--- resources/migrations/20180403131200-multiple-claims-in-pr.up.sql
+++ resources/migrations/20180403131200-multiple-claims-in-pr.up.sql
@@ -1,7 +1,7 @@
 ALTER TABLE pull_requests DROP CONSTRAINT pull_requests_pkey;
 ALTER TABLE pull_requests DROP CONSTRAINT pull_requests_pr_id_key;
 
-
+DELETE FROM pull_requests p WHERE NOT EXISTS (SELECT issue_id FROM issues i where p.issue_id=i.issue_id);
 ALTER TABLE pull_requests ADD CONSTRAINT pull_requests_pkey PRIMARY KEY (pr_id, issue_id);
 ALTER TABLE pull_requests ADD CONSTRAINT pull_requests_pr_id_key UNIQUE (pr_id, issue_id);
 ALTER TABLE pull_requests ADD CONSTRAINT pull_requests_fkey FOREIGN KEY (issue_id) REFERENCES issues(issue_id);
