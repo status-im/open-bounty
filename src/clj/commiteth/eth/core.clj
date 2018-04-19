687f29a59178bc22f36cee82d0997f3e3336ce20
Martin Klepsch <martinklepsch@googlemail.com>
2018-04-19 10:45:59 +0200

    only log eth-rpc result if internal-tx-id is present

--- src/clj/commiteth/eth/core.clj
+++ src/clj/commiteth/eth/core.clj
@@ -172,7 +172,9 @@
     (when internal-tx-id
       (log/infof "%s: eth-rpc %s" internal-tx-id method))
     (log/debugf "%s: eth-rpc req(%s) body: %s" internal-tx-id request-id body)
-    (log/infof "%s: eth-rpc req(%s) result: %s" internal-tx-id request-id result)
+    (if internal-tx-id
+      (log/infof "%s: eth-rpc req(%s) result: %s" internal-tx-id request-id result)
+      (log/debugf "no-tx-id: eth-rpc req(%s) result: %s" request-id result))
     (cond
       ;; Ignore any responses that have mismatching request ID
       (not= (:id result) request-id)
