08d56400ade6e025f36974c4b3f3c538f8480688
Rob Culliton <rob.culliton@gmail.com>
2018-05-08 13:38:25 -0400

    interceptor for payout_receipt within owner bounties

--- src/cljs/commiteth/interceptors.cljs
+++ src/cljs/commiteth/interceptors.cljs
@@ -3,28 +3,32 @@
             [re-frame.core :as rf]
             [clojure.data :as data]))
 
-(defn bounty-confirm-hashes [owner-bounties]
-  "for app-db representation of owner bounties, return a list of confirm hashes"
+(defn get-bounty-field [owner-bounties field-name]
+  "for app-db representation of owner bounties, return a list of just the relevant field"
   (-> owner-bounties
       vals
-      (->> (map #(:confirm_hash %)))))
+      (->> (map #(field-name %)))))
 
-(defn filter-updated-bounties [old-bounties new-bounties]
-  "filters collection of bounties to only those with a confirm has that has been recently set"
+(defn filter-updated-bounties [field-name new-bounties old-bounties]
+  "filters collection of bounties to only those with a field that has been recently set"
   (filter (fn [[issue-id owner-bounty]]
-            (let [new-confirm-hash (:confirm_hash owner-bounty)
-                  old-confirm-hash     (get-in old-bounties [issue-id :confirm_hash])]
+            (let [new-confirm-hash (field-name owner-bounty)
+                  old-confirm-hash     (get-in old-bounties [issue-id field-name])]
               (and (nil? old-confirm-hash) (some? new-confirm-hash))))
           new-bounties))
 
-(defn dispatch-bounty [bounty]
+(defn dispatch-confirm-payout [bounty]
   "dispatches a bounty via reframe dispatch"
   (rf/dispatch [:confirm-payout {:issue_id         (:issue-id bounty)
                                  :owner_address    (:owner_address bounty)
                                  :contract_address (:contract_address bounty)
                                  :confirm_hash     (:confirm_hash bounty)}]))
 
-(def confirm-hash-update
+(defn dispatch-remove-pending-revocation [bounty]
+  "dispatches a bounty via reframe dispatch"
+  (rf/dispatch [:remove-pending-revocation (:issue-id bounty)]))
+
+(def watch-confirm-hash
   "An interceptor which exaimines the event diff for `:load-owner-bounties`
   and dispatches a `confirm-payout` event when one of the owner bounties has
   its confirm_hash updated.
@@ -36,7 +40,7 @@
   https://github.com/Day8/re-frame/blob/master/docs/Interceptors.md"
 
   (rf/->interceptor
-    :id     :confirm-hash-update
+    :id     :watch-confirm-hash
     :after  (fn confirm-hash-update-after
               [context]
               (let [event-name          (-> context
@@ -49,16 +53,56 @@
                 ;; proceed when the change isn't caused by page load
                 (when (not-empty start-ob) 
                   (let [[only-before only-after both] (data/diff
-                                                       (bounty-confirm-hashes start-ob)
-                                                       (bounty-confirm-hashes end-ob))]
+                                                       (get-bounty-field start-ob :confirm_hash)
+                                                       (get-bounty-field end-ob :confirm_hash))]
                     ;; proceed when confirm hashes have changed and there is a pending revocation
                     (when (and only-after (not-empty pending-revocations))
                       (println "pending revocations are " pending-revocations)
                       (let [updated-bounties (->> end-ob
-                                                  (filter-updated-bounties start-ob)
+                                                  (filter-updated-bounties :confirm_hash start-ob)
                                                   vals)]
                         ;; for bounties which just had confirm hash set: perform
                         ;; dispatch side effect but interceptor must return context
                         (doseq [bounty updated-bounties]
-                          (dispatch-bounty bounty))))))
+                          (dispatch-confirm-payout bounty))))))
+                context))))
+
+
+(def watch-payout-receipt
+  "An interceptor which exaimines the event diff for `:load-owner-bounties`
+  and dispatches a `remove-pending-revocation` event when one of them has
+  its payout_receipt set.
+
+  *Warning* this inteceptor is only intended for use with the
+  `:load-owner-bounties` event
+
+  More information on re-frame interceptors can be found here:
+  https://github.com/Day8/re-frame/blob/master/docs/Interceptors.md"
+
+  (rf/->interceptor
+    :id     :watch-payout-receipt
+    :after  (fn payout-receipt-update-after
+              [context]
+              (let [event-name          (-> context
+                                            :coeffects
+                                            :event
+                                            first)
+                    pending-revocations (get-in context [:coeffects :db ::db/pending-revocations])
+                    start-ob            (get-in context [:coeffects :db :owner-bounties])
+                    end-ob              (get-in context [:effects :db :owner-bounties])]
+                ;; proceed when the change isn't caused by page load
+                (when (not-empty start-ob) 
+                  (let [[only-before only-after both] (data/diff
+                                                       (get-bounty-field start-ob :payout_receipt)
+                                                       (get-bounty-field end-ob :payout_receipt))]
+                    ;; proceed when payout_receipt has been updated and there is a pending revocation
+                    (when (and only-after (not-empty pending-revocations))
+                      (println "pending revocations are " pending-revocations)
+                      (let [updated-bounties (->> end-ob
+                                                  (filter-updated-bounties :payout_receipt start-ob)
+                                                  vals)]
+                        ;; for bounties which just had confirm hash set: perform
+                        ;; dispatch side effect but interceptor must return context
+                        (doseq [bounty updated-bounties]
+                          (dispatch-remove-pending-revocation bounty))))))
                 context))))
